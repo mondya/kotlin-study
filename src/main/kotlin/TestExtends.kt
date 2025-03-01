a22ed1c23915f3919952fe6f7fa0e640b1c530c3
xhh <xhh19990210@gmail.com>
2025-03-01 23:15:02 +0800

    kotlin study

--- src/main/kotlin/TestExtends.kt
+++ src/main/kotlin/TestExtends.kt
@@ -3,19 +3,30 @@ package org.example
 
 fun main() {
     val son = Son("son", 10)
-    son.parent()
-    println(son.name)
+//    son.parent()
+//    println(son.name)
+    baseEvent(son)
+    
+    val daughter = Daughter("daughter", 13)
+    baseEvent(daughter)
+}
+
+fun baseEvent(basePerson: Parent) {
+    println(basePerson.javaClass)
 }
 
 
-open class Parent(var name: String?, var age: Int?){
-    fun parent() {
+open class Parent(open var name: String?, open var age: Int?){
+    open fun parent() {
         println("parent")
     }
 }
 
 // 父类存在有参数的构造函数，子类必须调用父类的构造函数
-class Son(name: String, age: Int) : Parent(name, age) {
+class Son(override var name: String?, age: Int) : Parent(name, age) {
+    override fun parent() {
+        println("son do something")
+    }
 }
 
 class Daughter(name: String, age: Int) : Parent(name, age) {
